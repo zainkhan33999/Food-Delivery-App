96705c94e483ed1e99c72189d4b8b628d9652edf
zainkhan <khanzain33999@gmail.com>
2023-08-22 20:06:35 +0500

    code done

--- backend/Routers/createSeller.js
+++ backend/Routers/createSeller.js
@@ -5,46 +5,41 @@ const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken")
 const { body, validationResult } = require('express-validator');
 jwtSecret = "mynameiszainkhanilikecoffe"
-// Define the route to create a vendor
 router.post(
-  '/createvendor',
-  // body('vendorname', 'Incorrect Name').isLength({ min: 5 }),
-  // body('vendorpassword', 'Incorrect Password').isLength({ min: 5 }),
-  // body('vendornumber', 'Incorrect Number').isLength({ min: 5 }),
+  "/createvendor",
   async (req, res) => {
-    // const errors = validationResult(req);
-    // if (!errors.isEmpty()) {
-    //   return res.status(400).json({ errors: errors.array() });
-    // }
-
-    try {
-      const salt = await bcrypt.genSalt(10);
-      const securePassword = await bcrypt.hash(req.body.vendorpassword, salt);
-
-      const newVendor = new Vendor({
-        vendorname: req.body.vendorname,
-        vendorcompany: req.body.vendorcompany,
-        vendornumber: req.body.vendornumber,
-        vendorpassword: securePassword,
-        vendoraddress: req.body.vendoraddress,
-      });
-      const savedVendor = await newVendor.save();
+      const salt = await bcrypt.genSalt(10)
+      let securePassword = await bcrypt.hash(req.body.vendorpassword, salt)
+      try {
+        const salt = await bcrypt.genSalt(10);
+        const securePassword = await bcrypt.hash(req.body.vendorpassword, salt);
   
-      const data = {
-        user:{
-          id:newVendor.id
-        }
-      }
-      const sellToken = jwt.sign(data,jwtSecret)
-
-      res.status(201).json({success:true, vendor:savedVendor ,sellToken:sellToken});
+        const newVendor = new Vendor({
+          vendorname: req.body.vendorname,
+          vendorcompany: req.body.vendorcompany,
+          vendornumber: req.body.vendornumber,
+          vendorpassword: securePassword,
+          vendoraddress: req.body.vendoraddress,
+        });
+        const savedVendor = await newVendor.save();
     
-    } catch (error) {
-      console.log(error);
-      res.json({ success: false });
-    }
-    }
+        const data = {
+          user:{
+            id:newVendor.id
+          }
+        }
+        const sellToken = jwt.sign(data,jwtSecret)
   
-);
+        res.status(201).json({success:true, vendor:savedVendor ,sellToken:sellToken});
+      
+      } catch (error) {
+        console.log(error);
+        res.json({ success: false });
+      }
+      }
+    
+  );
+  
+  module.exports = router;
 
 module.exports = router;
--- src/screens/sellerSign.js
+++ src/screens/sellerSign.js
@@ -1,80 +1,88 @@
-import React, { useState } from 'react'
-import {useNavigate} from 'react-router-dom';
-import Navbar from '../components/Navbar'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
 const SellerSign = () => {
   const [credentials, setCredentials] = useState({
     vendorname: "",
     vendorcompany: "",
     vendornumber: "",
     vendorpassword: "",
-    vendoraddress:"",
-  
+    vendoraddress: "",
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/createvendor", { //it is saying bad request
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        vendorname: credentials.vendorname,
-        vendorcompany: credentials.vendorcompany,
-        vendornumber:credentials.vendornumber,
-        vendorpassword: credentials.vendorpassword,
-        vendoraddress: credentials.vendoraddress,
-      })
-    });
-    const json = await response.json()
-    console.log(json);
-  //   if (json.success) {
-  //     localStorage.setItem('sellToken', json.sellToken)
-  //     navigate("/");
+    try {
+      const response = await fetch("http://localhost:5000/api/createvendor", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          vendorname: credentials.vendorname,
+          vendorcompany: credentials.vendorcompany,
+          vendornumber: credentials.vendornumber,
+          vendorpassword: credentials.vendorpassword,
+          vendoraddress: credentials.vendoraddress,
+        })
+      });
 
-  //   }
-  //   else {
-  //     alert("Enter Valid Credentials")
-  //   }
-  // }
-  
- 
-  }
+      const json = await response.json();
+      console.log(json);
+
+      if (json.success) {
+        console.log("success true")
+        localStorage.setItem("sellToken", json.sellToken); 
+       
+      } else {
+        alert("Enter Valid Credentials");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  const onChange = (event) => {
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value
+    });
+  };
+
   return (
     <>
-    <Navbar/>
+       <Navbar/>
 <div className='container'>
 <form onSubmit={handleSubmit}>
   <div class="form-row ">
     <div class="form-group col-md-6">
       <label for="inputname">Vendor Name</label>
-      <input type="text" class="form-control" id="inputname" required placeholder="Vendor Name"/>
+      <input  onChange={onChange} name="vendorname" type="text" class="form-control" id="inputname" required placeholder="Vendor Name"/>
     </div>
     <div class="form-group col-md-6">
       <label for="inputcomname">Componey Name</label>
-      <input type="text" class="form-control" id="inputcomname"required placeholder="Componey Name"/>
+      <input onChange={onChange}  name="vendorcompany" type="text" class="form-control" id="inputcomname"required placeholder="Componey Name"/>
     </div>
     <div class="form-group col-md-6">
       <label for="inputnumber">Vendor Number</label>
-      <input type="number" class="form-control" id="inputnumber" required placeholder="Vendor Number"/>
+      <input onChange={onChange}  name="vendornumber" type="number" class="form-control" id="inputnumber" required placeholder="Vendor Number"/>
     </div>
 
     <div class="form-group col-md-6">
       <label for="inputPassword4">Vendor Password</label>
-      <input type="password" class="form-control" id="inputnumber" required placeholder="Password"/>
+      <input onChange={onChange}  name="vendorpassword" type="password" class="form-control" id="inputnumber" required placeholder="Password"/>
     </div> 
   </div>
   <div class="form-group">
     <label for="inputAddress">Resturent Address</label>
-    <input type="text" class="form-control" id="inputAddress" required placeholder="1234 Main St"/>
+    <input onChange={onChange} name="vendoraddress" type="text" class="form-control" id="inputAddress" required placeholder="1234 Main St"/>
   <button type="submit" class="btn btn-primary">Sign in</button>
   </div>    
 </form>
     </div>
       </>
+    
+  );
+};
 
-
-  )
-}
-
-export default SellerSign
+export default SellerSign;
